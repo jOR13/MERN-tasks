28763b64554584a9210481f1fb94f99f5f91a57e
jor13 <ochoarj13@hotmail.com>
2020-10-08 11:33:29 -0600

    .

--- servidor/index.js
+++ servidor/index.js
@@ -35,6 +35,10 @@ app.use('/api/tareas', require('./routes/tareas'));
 
 
 //arrancar la app
-app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor esta funcionando en el puerto ${port}`);
-})
+// app.listen(port, '0.0.0.0', () => {
+//     console.log(`El servidor esta funcionando en el puerto ${port}`);
+// })
+
+app.listen(5000, function(){
+    console.log('listening on *:5000');
+  });
